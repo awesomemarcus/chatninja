1d9ddada1d84bd3331c3dccb695513dc08c736c5
aj <aj@snapzio.com>
2016-06-01 15:24:12 +0800

    bubles bottom

--- client/modules/messages/components/messages_box.jsx
+++ client/modules/messages/components/messages_box.jsx
@@ -1,25 +1,16 @@
 import React from 'react';
 
 // const MessagesBox = ({msg}) => (
-//   <div id="main">
 //     <ul>
 //       <li><p className="bubble you">{msg.to}</p></li>
 //       <li><p className="bubble me">{msg.from}</p></li>
 //     </ul>
-//   </div>
+//
 // );
 
 const MessagesBox = () => (
   <div id="main">
     <ul>
-      <li><p className="bubble you">Hi Marcus!</p></li>
-      <li><p className="bubble me">Zup?</p></li>
-      <li><p className="bubble you">Hi Marcus!</p></li>
-      <li><p className="bubble me">Zup?</p></li>
-      <li><p className="bubble you">Hi Marcus!</p></li>
-      <li><p className="bubble me">Zup?</p></li>
-      <li><p className="bubble you">Hi Marcus!</p></li>
-      <li><p className="bubble me">Zup?</p></li>
       <li><p className="bubble you">Hi Marcus!</p></li>
       <li><p className="bubble me">Zup?</p></li>
       <li><p className="bubble you">Hi Marcus!</p></li>
