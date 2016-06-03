b14b4592c822e1d51df736fe534ed3b6d751804e
Mark Jason Aquiatin <marcus@snapzio.com>
2016-06-03 12:52:39 +0800

    deleted unnecessary files

--- client/modules/users/containers/users_list.js
+++ client/modules/users/containers/users_list.js
@@ -5,7 +5,7 @@ import UsersList from '../components/users_list.jsx';
 export const composer = ({context}, onData) => {
   const {Meteor} = context();
 
-  if(Meteor.subscribe('allUsers', Meteor.userId).ready()){
+  if(Meteor.subscribe('allUsers').ready()){
    const users = Meteor.users.find({_id:{$ne:Meteor.userId()}},{sort: {'status.online': -1, 'profile.username': 1}}).fetch();
 
    onData(null, {users});
